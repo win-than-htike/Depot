32dbb1ed2365562cf4b6ef81e7fd54468edb147a
Win Than Htike <13495028+win-than-htike@users.noreply.github.com>
2025-01-29 01:45:08 +0700

    Add Validation and test cases

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   
   test "product attributes must not be empty" do
     product = Product.new
@@ -34,6 +35,40 @@ class ProductTest < ActiveSupport::TestCase
       price: 1,
       image_url: image_url
     )
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+             http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    ok.each do |image_url|
+      assert new_product(image_url).valid?,
+        "#{image_url} shouldn't be invalid"
+    end
+
+    bad.each do |image_url|
+      assert new_product(image_url).invalid?,
+        "#{image_url} shouldn't be valid"
+    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
+
+  test "product is not valid without a unique title - i18n" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal [I18n.translate('errors.messages.taken')],
+                 product.errors[:title]
   end
 
 end
