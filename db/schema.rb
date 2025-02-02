51085ac6e09d1bc349bce22ec3746f2de024c920
Win Than Htike <13495028+win-than-htike@users.noreply.github.com>
2025-02-02 23:43:18 +0700

    Add the sum of cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_31_105605) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_02_102136) do
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_31_105605) do
     t.integer "cart_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
